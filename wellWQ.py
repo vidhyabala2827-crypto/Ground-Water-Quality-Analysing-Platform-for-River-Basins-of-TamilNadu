69a58929fb33f0aa7e8478d9b5e53696729e73f5
vidhyabala2827-crypto <vidhyabala2827@gmail.com>
2025-09-30 13:49:01 +0530

    Update wellWQ.py

--- wellWQ.py
+++ wellWQ.py
@@ -25,7 +25,7 @@ st.markdown("""
 # -----------------
 # App Title
 # -----------------
-st.markdown("<h1 style='text-align: center; color: #003366;'>Ground water quality Analysis- River Nasins of TamilNadu</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #003366;'>Ground water quality Analysis- River Basins of TamilNadu</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; font-style: italic; color: #0059b3;'>\"We never know the worth of water till the well is dry\"</h4>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; color: #003366;'>- Thomas Fuller</h5>", unsafe_allow_html=True)
 st.image(
@@ -196,6 +196,7 @@ if upload_clicked:
     if uploaded_file:
         df = load_data(uploaded_file)
         st.success("Your data is loaded! You can now use the selections above.")
+
 
 
 
