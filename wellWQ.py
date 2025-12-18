a7b675e8820fc8c2bd95ed3bfd3d0ad9e2d3892d
vidhyabala2827-crypto <vidhyabala2827@gmail.com>
2025-12-18 15:21:55 +0530

    Update wellWQ.py

--- wellWQ.py
+++ wellWQ.py
@@ -318,12 +318,14 @@ if menu in ["Descriptive Statistics", "Visualizations", "Correlation Analysis"]:
             st.pyplot(plt)
 
     elif menu == "Correlation Analysis":
-            st.subheader("Correlation Analysis")
-            corr = filtered[parameters].corr()
-            plt.figure(figsize=(12, 8))
-            sns.heatmap(corr, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
-            plt.yticks(rotation=0)
-            st.pyplot(plt)
+        corr = filtered[parameters].dropna().corr(method=st.sidebar.radio("Correlation Method", ["pearson","spearman"]))
+        st.subheader("Correlation Matrix")
+        st.dataframe(corr)
+        plt.figure(figsize=(12,8))
+        sns.heatmap(corr, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+        st.pyplot(plt)
+        plt.yticks(rotation=0)
+        st.pyplot(plt)
             
 
 # =========================================================
@@ -345,6 +347,7 @@ if upload_clicked:
     if file:
         df = load_data(file)
         st.success("File uploaded successfully.")
+
 
 
 
